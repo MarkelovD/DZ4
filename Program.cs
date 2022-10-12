09fe81cd45d423285170a142d973084f762c79cb
Dmitry <markelov16@gmail.com>
2022-10-12 18:56:48 +0500

    задача 2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿// задача 1
-int VozvStep (int chislo, int stepen){ // создание метода возведение в степень
-int rez=chislo;
-for (int i=1; i<stepen;i++){// цикл перемножения на себя
-    rez*=rez;
-}
-return rez;// возврат результата
+// int VozvStep (int chislo, int stepen){ // создание метода возведение в степень
+// int rez=chislo;
+// for (int i=1; i<stepen;i++){// цикл перемножения на себя
+//     rez*=rez;
+// }
+// return rez;// возврат результата
+// }
+// Console.WriteLine("введите число");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("введите степень числа");
+// int B = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"{VozvStep(A,B)}");
+
+//задача 2
+int SummaChisel(int chislo){
+    int summ =0;
+    while (chislo!=0){
+     summ+=chislo%10;
+     chislo=chislo/10;
+    }
+    return summ;
 }
 Console.WriteLine("введите число");
 int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine("введите степень числа");
-int B = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"{VozvStep(A,B)}");
+Console.WriteLine($"сумма чисел входящих в число {A} равна {SummaChisel(A)}");
