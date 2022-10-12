d170c78ad592332a0c52fafc206d613402bb7b56
Dmitry <markelov16@gmail.com>
2022-10-12 19:31:42 +0500

    задача 3

--- Program.cs
+++ Program.cs
@@ -13,14 +13,28 @@
 // Console.WriteLine($"{VozvStep(A,B)}");
 
 //задача 2
-int SummaChisel(int chislo){
-    int summ =0;
-    while (chislo!=0){
-     summ+=chislo%10;
-     chislo=chislo/10;
-    }
-    return summ;
-}
-Console.WriteLine("введите число");
-int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"сумма чисел входящих в число {A} равна {SummaChisel(A)}");
+// int SummaChisel(int chislo){
+//     int summ =0;
+//     while (chislo!=0){
+//      summ+=chislo%10;
+//      chislo=chislo/10;
+//     }
+//     return summ;
+// }
+// Console.WriteLine("введите число");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"сумма чисел входящих в число {A} равна {SummaChisel(A)}");
+
+//задача 3
+
+int [] array = Massiv(8);// обращение к методу
+Console.WriteLine($"[{String.Join(",",array)}]");// вывод метода (первые кавычки-разделитель, array-элементы массива)
+int[] Massiv(int size)// метод заполнения массива
+{
+    int [] result = new int [size];// объявление массива 
+    for (int i = 0; i<size; i++)// цикл заполнения массива
+ {
+    result [i] = new Random().Next(9);// заполнение ячейки массива случайным числом от 1 до 8
+ }
+    return result;
+ }
