cde3910c36a02c18007fcd2c9f8c20f5b37ab632
Dmitry <markelov16@gmail.com>
2022-10-12 18:39:41 +0500

    задача 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// задача 1
+int VozvStep (int chislo, int stepen){ // создание метода возведение в степень
+int rez=chislo;
+for (int i=1; i<stepen;i++){// цикл перемножения на себя
+    rez*=rez;
+}
+return rez;// возврат результата
+}
+Console.WriteLine("введите число");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите степень числа");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{VozvStep(A,B)}");
